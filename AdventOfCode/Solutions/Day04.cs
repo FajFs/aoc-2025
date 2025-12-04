9e03ee64ef51cc75da4cb7c8fd4105ce73fda1b9
Felix Andersson Johansson <felix.andersson.johansson@prevas.se>
2025-12-04 07:19:38 +0100

    Day 4

--- AdventOfCode/Solutions/Day04.cs
+++ AdventOfCode/Solutions/Day04.cs
@@ -6,20 +6,80 @@ public class Day04_2025(AdventOfCodeClient client)
     public async Task<string> SolvePart1()
     {
         var input = await client.GetInputAsync(2025, 4);
+        var result = input
+            .ParseInput()
+            .CountAndRemoveAdjecentRollsOfPaper()
+            .Count();
 
-        return input.ToString();
+        return result.ToString();
     }
 
     public async Task<string> SolvePart2()
     {
         var input = await client.GetInputAsync(2025, 4);
+        var result = input
+            .ParseInput()
+            .CountAndRemoveAdjecentRollsOfPaperRecursively()
+            .Sum();
 
-        return input.ToString();
+        return result.ToString();
     }
 }
 
 
 file static class Day04_2025_Extensions
 {
+    public static (int row, int col)[] directions =
+    [
+            (0, 1),  // Right
+            (0, -1), // Left
+            (1, 0),  // Down
+            (-1, 0), // Up
+            (1, 1),  // Down-Right
+            (1, -1), // Down-Left
+            (-1, 1), // Up-Right
+            (-1, -1) // Up-Left
+    ];
 
+    public static List<List<char>> ParseInput(this IEnumerable<string> lines)
+        => [.. lines.Select(x => x.ToList())];
+
+    public static IEnumerable<int> CountAndRemoveAdjecentRollsOfPaperRecursively(this List<List<char>> grid)
+    {
+        var removed = grid.CountAndRemoveAdjecentRollsOfPaper().ToList();
+         return removed switch
+        {
+            [] => [],
+            _ => [.. removed, .. grid.CountAndRemoveAdjecentRollsOfPaperRecursively()] // concatenate lists of ints and concatenate recursive call until empty
+        };
+    }
+
+    public static IEnumerable<int> CountAndRemoveAdjecentRollsOfPaper(this List<List<char>> grid)
+    {
+        var paperRollsToRemove = new List<(int row, int col)>();
+        for (var row = 0; row < grid.Count; row++)
+            for (var col = 0; col < grid[row].Count; col++)
+            {
+                var position = grid[row][col];
+                if (position is '.')
+                    continue;
+
+                var totalAdjecentPaperRolls = directions.Count(direction =>
+                {
+                    var (r , c) = (row + direction.row, col + direction.col);
+                    return r >= 0 && r < grid.Count &&
+                           c >= 0 && c < grid[r].Count &&
+                           grid[r][c] == position;
+                });
+
+                if (totalAdjecentPaperRolls < 4)
+                    paperRollsToRemove.Add((row, col));
+            }
+
+        foreach (var (row, col) in paperRollsToRemove)
+        {
+            grid[row][col] = '.';
+            yield return 1;
+        }
+    }
 }
---
+++ AdventOfCode/Solutions/Day05.cs
@@ -0,0 +1,27 @@
+namespace AdventOfCode.Solutions;
+
+public class Day05_2025(AdventOfCodeClient client) 
+    : ISolution
+{
+    public async Task<string> SolvePart1()
+    {
+        var input = await client.GetInputAsync(2025, 5);
+        var result = input;
+
+        return result.ToString();
+    }
+
+    public async Task<string> SolvePart2()
+    {
+        var input = await client.GetInputAsync(2025, 5);
+        var result = input;
+
+        return result.ToString();
+    }
+}
+
+
+file static class Day05_2025_Extensions
+{
+  
+}
