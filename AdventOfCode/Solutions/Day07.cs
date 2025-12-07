e968bff34ce19ce60653e8681fc02f20dabb313d
Felix Andersson Johansson <felix.andersson.johansson@prevas.se>
2025-12-07 11:53:50 +0100

    Day 7

--- AdventOfCode/Solutions/Day07.cs
+++ AdventOfCode/Solutions/Day07.cs
@@ -1,5 +1,3 @@
-using System.Collections.Frozen;
-
 namespace AdventOfCode.Solutions;
 
 public class Day07_2025(AdventOfCodeClient client) 
@@ -8,7 +6,10 @@ public class Day07_2025(AdventOfCodeClient client)
     public async Task<string> SolvePart1()
     {
         var input = await client.GetInputAsync(2025, 7);
-        var result = input;
+        var result = input
+            .ParseInput()
+            .PropagateBeams()
+            .BeamsSplitCount;
 
         return result.ToString();
     }
@@ -16,7 +17,10 @@ public class Day07_2025(AdventOfCodeClient client)
     public async Task<string> SolvePart2()
     {
         var input = await client.GetInputAsync(2025, 7);
-        var result = input;
+        var result = input
+            .ParseInput()
+            .PropagateBeams()
+            .TotalActiveBeams;
 
         return result.ToString();
     }
@@ -24,5 +28,31 @@ public class Day07_2025(AdventOfCodeClient client)
 
 file static class Day07_2025_Extensions
 {
-    
+    public static List<List<char>> ParseInput(this IEnumerable<string> lines)
+        => [.. lines.Select(x => x.Select(y => y).ToList())];
+
+    public static (int BeamsSplitCount, double TotalActiveBeams) PropagateBeams(
+        this List<List<char>> points)
+    {
+        var beamsSplitCount = 0;
+
+        var beams = new double[points[0].Count];
+        beams[points[0].IndexOf('S')] = 1;
+
+        for (var row = 1; row < points.Count; row++)
+            for (var col = 0; col < points[row].Count; col++)
+            {
+                var activeBeams = beams[col];
+                if (activeBeams == 0 || points[row][col] != '^')
+                    continue;
+
+                beams[col - 1] += activeBeams;
+                beams[col + 1] += activeBeams;
+                beams[col] = 0;
+
+                beamsSplitCount++;
+            }
+
+        return (beamsSplitCount, beams.Sum());
+    }
 }
---
+++ AdventOfCode/Solutions/Day08.cs
@@ -0,0 +1,26 @@
+namespace AdventOfCode.Solutions;
+
+public class Day08_2025(AdventOfCodeClient client) 
+    : ISolution
+{
+    public async Task<string> SolvePart1()
+    {
+        var input = await client.GetInputAsync(2025, 8);
+        var result = input;
+
+        return result.ToString();
+    }
+
+    public async Task<string> SolvePart2()
+    {
+        var input = await client.GetInputAsync(2025, 8);
+        var result = input;
+
+        return result.ToString();
+    }
+}
+
+file static class Day08_2025_Extensions
+{
+
+}
